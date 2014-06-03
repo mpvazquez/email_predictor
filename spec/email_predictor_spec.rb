18e06ddec362034d8502e04dd6166e94a461a50b
Marco Pretell-Vazquez <marco.pvazquez@gmail.com>
2014-06-03 15:20:09 -0400

    started writing base tests of email predction

---
+++ spec/email_predictor_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+require_relative '../lib/email_predictor'
+
+describe "predict email addresses" do
+  describe "by accepting input of advisors' full-name and email" do
+
+  end
+
+  describe "by analyzing past dataset of advisor emails" do
+
+  end
+
+  describe "returns predicted email address based on past advisor emails" do
+
+  end
+end 
